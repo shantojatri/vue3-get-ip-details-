9828248098f6fedfd2e4c978a1016c9b5d33162b
Hasibul Hasan Shanto <117150953+shantojatri@users.noreply.github.com>
2023-06-15 17:28:52 +0600

    Update some code and added bangla for DB

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import axios from "axios";
-const detailsValue = ref<String>('');
-const getIp = ref<String>('');
 
-const getIpAddress = async() => { 
-  await axios.get(`https://api.ipify.org/?format=json`)
-    .then((res) => {
-    console.log(res.data.ip)  
-    getIp.value = res.data.ip
-  });  
-}
+const detailsValue = ref<Array>([]);
 
-const getDetails2 = async (ip) => {
-  await axios.get(`http://ip-api.com/json/${ip}`)
-  .then((res) => {  
-    detailsValue.value = res.data 
-  });  
-}
+// const getIp = ref<String>('');
+// const getIpAddress = async() => { 
+//   await axios.get(`https://api.ipify.org/?format=json`)
+//     .then((res) => {
+//     console.log(res.data.ip)  
+//     getIp.value = res.data.ip
+//   });  
+// }
 
-const getDetails = async () => {
-  await axios.get(`http://ip-api.com/json/`)
-  .then((res) => {  
-    detailsValue.value = res.data 
-  });  
-}
+// const getDetails2 = async (ip) => {
+//   await axios.get(`http://ip-api.com/json/${ip}`)
+//   .then((res) => {  
+//     detailsValue.value = res.data 
+//   });  
+// }
 
 // const getAll2 = () => { 
 //   Promise.all([getIpAddress()]).then(() => {
@@ -33,6 +27,14 @@ const getDetails = async () => {
 //   .finally(()=> console.log('Ended2'));
 // }
 // getAll2();
+
+
+const getDetails = async () => {
+  await axios.get(`http://ip-api.com/json/`)
+  .then((res) => {  
+    detailsValue.value = res.data 
+  });  
+}
 
 /**
  * This function is way better than getAll2, for getting IP details
@@ -49,20 +51,18 @@ getAll();
     <div class="wrapper">  
       <div v-if="detailsValue?.status === 'success'">
         <div v-if="detailsValue?.countryCode === 'BD'">
-          Country: Bangladesh
-          Language: Bangla
+          দেশঃ বাংলাদেশ 
+          ভাষাঃ বাংলা 
         </div>
         <div v-else>
           Country: {{ detailsValue?.country }}
           Language: English
         </div>
       </div>
-      <h1>Get Ip adress: </h1>
-      <p>{{ detailsValue }}</p>
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+      <h1>Get IP Address Details: </h1>
+      <p>{{ detailsValue }}</p>
+
+    </div>
+  </header> 
 </template>
